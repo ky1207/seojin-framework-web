b0f997d38db801ec3f7d54fdc60ed1490071f79b
kskim <kskim@seojinsys.co.kr>
2022-11-29 16:12:46 +0900

    SJOneLayout Button slot 추가

--- components/ui/layouts/SJOneLayout.vue
+++ components/ui/layouts/SJOneLayout.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="#">Home</a>
-          </li>
-          <li class="breadcrumb-item active">
-            Dashboard
-          </li>
-        </ol>
-      </nav>
+    <div class="row pagetitle mt-1">
+      <div class="col">
+        <h1>Dashboard</h1>
+        <nav>
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
+            <li class="breadcrumb-item active">
+              Dashboard
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <div class="col-auto">
+        <slot name="master-btn" />
+      </div>
     </div><!-- End Page Title -->
     <div class="card">
       <div class="card-body">
