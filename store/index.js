3ab3802c8e70a70683d0eb6994279a5c2f9c22c9
kskim <kskim@seojinsys.co.kr>
2022-12-09 15:33:35 +0900

    자동 닫힘.. 3초로 변경

--- store/index.js
+++ store/index.js
@@ -24,11 +24,9 @@ export const actions = {
 
   showNotify (context, notify) {
     context.commit('showNotify', notify)
-    // serverside에서 client에 vuex 값 동기화 하지 못함
-    // 처리되지  못하는  경우, TopNodify.vue에서 별도 처리함
     setTimeout(function () {
       context.commit('closeNotify')
-    }, 2000)
+    }, 3000)
   },
   closeNotify (context) {
     context.commit('closeNotify')
