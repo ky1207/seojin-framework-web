2eae7b3d9555c3afc5ce3428633f1da905253421
kskim <kskim@seojinsys.co.kr>
2023-01-13 09:34:51 +0900

    IP체크

--- plugins/client.js
+++ plugins/client.js
@@ -3,10 +3,7 @@
  * 최초 서버 request시
  */
 
-export default ({ req, store }) => {
-  if (process.server) {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress
-    store.commit('client/setIp', ip)
-  }
-  console.log('ipCheck')
+export default async ({ $axios, store, env }) => {
+  const result = await $axios.get(env.API_SERVER + '/api/v1.0/common/myIp')
+  store.commit('client/setIp', result.data)
 }
