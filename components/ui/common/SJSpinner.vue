a7064e0ecc7384fa8abd8b9a2b7c13850b54cea8
kskim <kskim@seojinsys.co.kr>
2023-02-21 11:27:25 +0900

    RGB 컬러 변경

--- components/ui/common/SJSpinner.vue
+++ components/ui/common/SJSpinner.vue
@@ -32,7 +32,7 @@ export default {
   position:fixed;
   width:100%;
   left:0;right:0;top:0;bottom:0;
-  background-color: rgba(0,0,0,0.7);
+  background-color: rgba(0,0,0,0);
   z-index:9999;
   display:block;
 }
