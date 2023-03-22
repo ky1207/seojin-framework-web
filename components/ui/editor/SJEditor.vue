3c82098af79119cf6aa4a182e3a30cc517b3583b
kskim <kskim@seojinsys.co.kr>
2023-03-22 17:07:32 +0900

    ckeditor로 변경

--- components/ui/editor/SJEditor.vue
+++ components/ui/editor/SJEditor.vue
@@ -7,22 +7,25 @@
     tag="div"
     :disabled="disabledValidation"
   >
-    <Editor
+    <ckeditor
       v-bind="$attrs"
       ref="editor"
-      :options="editorOptions"
-      initial-edit-type="wysiwyg"
+      v-model="innerValue"
+      :editor="editor"
       :class="disabledValidation?'':classes"
-      :aria-describedby="id+'-feedback'"
+      :config="editorConfig"
       v-on="$listeners"
       @blur="handleInput"
     />
+
     <span :id="id+'-feedback'" class="invalid-feedback">
       {{ errors[0] }}
     </span>
   </ValidationProvider>
 </template>
 <script>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
 export default {
   props: {
     value: {
@@ -52,25 +55,41 @@ export default {
   },
   data () {
     return {
-      editorOptions: {
-        hideModeSwitch: true
+      editor: ClassicEditor,
+      editorConfig: {
+        toolbar: {
+          items: ['heading', '|', 'bold', 'sourceEditing', 'italic', 'bulletedList', 'numberedList', '|']
+        },
+        language: 'en'
       }
     }
   },
-  mounted () {
-    this.$refs.editor.invoke('setHTML', this.$options.filters.unescapeHTML(this.value))
+  computed: {
+    innerValue: {
+      get () {
+        return this.$options.filters.unescapeHTML(this.value)
+      },
+      set (_val) {
+        this.$emit('input', _val)
+      }
+    }
   },
   methods: {
     handleInput (e) {
-      const html = this.$refs.editor.invoke('getHTML')
-      this.$emit('input', html)
+      // const html = this.$refs.editor.invoke('getHTML')
+      // this.$emit('input', html)
       this.validate()
     },
     validate () {
-      this.$refs.provider.value = this.$refs.editor.invoke('getMarkdown')
+      // this.$refs.provider.value = this.$refs.editor.invoke('getMarkdown')
       this.$refs.provider.validate()
     }
   }
 }
 
 </script>
+<style>
+.ck-editor__editable {
+  height: 200px;
+}
+</style>
