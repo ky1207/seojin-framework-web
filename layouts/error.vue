78e47d380452efe17977d7b2a49b0f83a9e94525
yeonji0 <kyj0279@gmail.com>
2023-04-25 13:30:37 +0900

    error페이지 수정

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,41 +1,52 @@
 <template>
-  <div style="height: 100%">
-    <div class="container">
-      <div><img src="~/assets/images/error.png"></div>
-      <div class="error-layer">
-        <div class="error-header" style="font-size: 60px;color: #007DB6;font-weight: bold">
-          페이지를 찾을 수 없습니다.
-        </div>
-        <div class="error-body" style="font-size: 18px;line-height: 2.1">
-          <p>페이지가 존재하지 않거나 사용할 수 없는 페이지입니다. <br> 입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
-        </div>
-        <div class="error-footer">
-          <button class="btn-alert" type="button" @click="goToMain">
-            메인으로 돌아가기
-          </button>
-        </div>
+  <div style="height: 100%;display:flex; justify-content:center; align-items: center;">
+    <div style="margin-right: 20px;">
+      <img src="~assets/images/error.png">
+    </div>
+    <div class="error-layer">
+      <div class="error-header" style="font-size: 50px;color: #007DB6;font-weight: bold">
+        <h1 style="font-size: 80px">
+          {{ error.statusCode }}
+        </h1>페이지를 찾을 수 없습니다.
       </div>
+      <div class="error-body">
+        <!--{{ error.message }}-->
+        <p style="font-size: 18px;line-height:1.5">
+          페이지가 존재하지 않거나 사용할 수 없는 페이지입니다. <br> 입력하신 주소가 정확한지 다시 한 번 확인해주세요.
+        </p>
+      </div>
+      <button class="btn-alert" type="button" @click="goToMain">
+        메인으로 돌아가기
+      </button>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   layout: 'empty',
   props: ['error'],
   methods: {
     goToMain () {
-      this.$router.back()
+      if (this.$route.path === '/') {
+        window.open('/', '_self')
+      } else {
+        this.$router.push('/')
+      }
     }
   }
 }
 </script>
 <style>
-.container {
-  display: flex;
-  justify-content: center;
-  align-items:center;
-  height: 100vh;
+#__layout>div{
+  height: 100%;}
+img{
+  width: 300px;
+  height: 300px;}
+.container img{
+  width: 150px;
+  height: 150px;}
+h1 {
+  font-size: 20px;
 }
-.error-layer{
-  margin-left: 30px;}
 </style>
