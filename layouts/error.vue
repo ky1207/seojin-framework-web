eda13f61e10ed3c80dd2e16f0e44edaf789b0345
yeonji0 <kyj0279@gmail.com>
2023-04-25 10:16:29 +0900

    error 페이지 적용

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,28 +1,46 @@
 <template>
-  <SJOneLayout disabled-navigator>
-    <div class="page-wrap d-flex flex-row align-items-center">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-12 text-center">
-            <span class="display-1 d-block"> {{ error.statusCode }}</span>
-            <div class="mb-4 lead">
-              {{ error.message }}
-            </div>
-          </div>
+  <div style="height: 100%">
+    <div class="container">
+      <div><img src="~/assets/images/error.png"></div>
+      <div class="error-layer">
+        <div class="error-header" style="font-size: 60px;color: #007DB6;font-weight: bold">
+          페이지를 찾을 수 없습니다.
+        </div>
+        <div class="error-body" style="font-size: 18px;line-height: 2.1">
+          <p>페이지가 존재하지 않거나 사용할 수 없는 페이지입니다. <br> 입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
+        </div>
+        <div class="error-footer">
+          <button class="btn-alert" type="button" @click="goToMain">
+            메인으로 돌아가기
+          </button>
         </div>
       </div>
     </div>
-  </SJOneLayout>
+  </div>
 </template>
 
+Diff
+Copy
+Insert
+New
 <script>
 export default {
   layout: 'empty',
-  props: ['error']
+  props: ['error'],
+  methods: {
+    goToMain () {
+      this.$router.back()
+    }
+  }
 }
 </script>
 <style>
-h1 {
-  font-size: 20px;
+.container {
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  height: 100vh;
 }
+.error-layer{
+  margin-left: 30px;}
 </style>
