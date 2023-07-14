b21d18b3af85263aeceb7ac45cbb4acf4ad14a11
yeonji <yeonji@seojinsys.co.kr>
2023-07-14 15:10:41 +0900

    md109,md110 추가

---
+++ pages/md/md109.vue
@@ -0,0 +1,77 @@
+<template>
+  <SJSearchOneLayout>
+    <template #master-btn>
+      <SJPageButtons :action="ACTION" />
+    </template>
+    <template>
+      <SJSearchField label="채번유형코드">
+        <SJInput id="md1" v-model="search.md1" name="md1" />
+      </SJSearchField>
+    </template>
+    <template #bodyTitle>
+      <SJTitle title="수량포맷정보">
+        <button class="btn-white-bg">
+          추가
+        </button>
+        <button class="btn-white-bg">
+          삭제
+        </button>
+      </SJTitle>
+    </template>
+    <template #body>
+      <SJGrid
+        ref="grid"
+        v-model="grid.data"
+        :columns="grid.columns"
+      />
+    </template>
+  </SJSearchOneLayout>
+</template>
+
+<script>
+import { ACTION } from '~/mixins'
+
+export default {
+  mixins: [ACTION],
+  data () {
+    return {
+      search: {},
+      grid: {
+        data: {},
+        columns: [
+          { name: '채번유형코드' },
+          { name: '채번명' },
+          { name: '최대길이' },
+          { name: '접두사' },
+          { name: '날짜유형' },
+          { name: '채번증가수' },
+          { name: '숫자최대길이' },
+          { name: '상세정보' },
+          { name: '날짜정보' },
+          { name: '숫자' },
+          { name: '최종채번번호' }
+        ]
+      }
+    }
+  },
+  created () {
+  },
+  methods: {
+    ACTION_REGISTRY () {
+      const fnc = this
+      return {
+        async searchClick () {
+          await fnc.$notify.info('조회')
+        },
+        async saveClick () {
+          await fnc.$notify.success('저장')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/md/md110.vue
@@ -0,0 +1,186 @@
+<template>
+  <SJSearchOneLayout>
+    <template #master-btn>
+      <SJPageButtons :action="ACTION" />
+    </template>
+    <template>
+      <SJSearchField label="품목유형">
+        <SJSelect
+          id="md1"
+          name="md1"
+          :options="MD_01"
+        />
+      </SJSearchField>
+      <SJSearchField label="품목분류">
+        <SJInput id="md2" v-model="search.md2" name="md2" />
+      </SJSearchField>
+      <SJSearchField label="규격">
+        <SJInput id="md3" v-model="search.md3" name="md3" />
+      </SJSearchField>
+      <SJSearchField label="CE!구분">
+        <SJSelect
+          id="md4"
+          name="md4"
+          :options="MD_02"
+        />
+      </SJSearchField>
+      <SJSearchField label="구매L/T">
+        <SJInput id="md5" v-model="search.md5" name="md5" />
+      </SJSearchField>
+      <SJSearchField label="규격">
+        <SJInput id="md6" v-model="search.md6" name="md6" />
+      </SJSearchField>
+      <SJSearchField label="거래처">
+        <SJInput id="md7" v-model="search.md7" name="md7" />
+      </SJSearchField>
+      <SJSearchField label="사용여부">
+        <SJSelect
+          id="md8"
+          name="md8"
+          :options="MD_03"
+        />
+      </SJSearchField>
+      <SJSearchField label="크리티컬여부">
+        <SJSelect
+          id="md9"
+          name="md9"
+          :options="MD_02"
+        />
+      </SJSearchField>
+      <SJSearchField label="크리티컬등급">
+        <SJInput id="md10" v-model="search.md10" name="md10" />
+      </SJSearchField>
+    </template>
+    <template #bodyTitle>
+      <SJTitle title="품목목록">
+        <button class="btn-white-bg">
+          추가
+        </button>
+        <button class="btn-white-bg">
+          삭제
+        </button>
+      </SJTitle>
+    </template>
+    <template #body>
+      <SJGrid
+        ref="grid"
+        v-model="grid.data"
+        :columns="grid.columns"
+      />
+    </template>
+  </SJSearchOneLayout>
+</template>
+
+<script>
+import { ACTION } from '~/mixins'
+
+export default {
+  mixins: [ACTION],
+  data () {
+    return {
+      search: {},
+      grid: {
+        data: {},
+        columns: [
+          { name: '품목유형' },
+          { name: '품목코드' },
+          { name: '품목명' },
+          { name: 'REV번호' },
+          { name: '크리티컬여부' },
+          { name: '크리티컬등급' },
+          { name: '품목대분류' },
+          { name: '품목소분류' },
+          { name: '기준단위' },
+          { name: '거래처명' },
+          { name: '단가' },
+          { name: '조달구분' },
+          { name: 'Country or Origin' },
+          { name: '가공시간' }
+        ]
+      }
+    }
+  },
+  created () {
+    this.MD_01 = this.getMD01()
+    this.MD_02 = this.getMD02()
+    this.MD_03 = this.getMD03()
+  },
+  methods: {
+    ACTION_REGISTRY () {
+      const fnc = this
+      return {
+        async searchClick () {
+          await fnc.$notify.info('조회')
+        },
+        async saveClick () {
+          await fnc.$notify.success('저장')
+        },
+        async f1Click () {
+          await fnc.$notify.success('수정')
+        },
+        f1Label: '수정'
+      }
+    },
+    getMD01 () {
+      return [
+        {
+          text: '제품',
+          value: '제품'
+        },
+        {
+          text: '반제품',
+          value: '반제품'
+        },
+        {
+          text: '원재료',
+          value: '원재료'
+        },
+        {
+          text: '저장품',
+          value: '저장품'
+        },
+        {
+          text: '...',
+          value: '...'
+        }
+      ]
+    },
+    getMD02 () {
+      return [
+        {
+          text: '전체',
+          value: '전체'
+        },
+        {
+          text: 'Y',
+          value: 'Y'
+        },
+        {
+          text: 'N',
+          value: 'N'
+        }
+      ]
+    },
+    getMD03 () {
+      return [
+        {
+          text: '전체',
+          value: '전체'
+        },
+        {
+          text: '사용',
+          value: '사용'
+        },
+        {
+          text: '미사용',
+          value: '미사용'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
