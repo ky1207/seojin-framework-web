186764d6d5736fc919ac92240df2ff24281bfb01
yeonji <yeonji@seojinsys.co.kr>
2023-07-14 12:57:35 +0900

    md107,md108 추가

---
+++ pages/md/md107.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>md107</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/md/md108.vue
@@ -0,0 +1,75 @@
+<template>
+  <SJSearchOneLayout>
+    <template #master-btn>
+      <SJPageButtons :action="ACTION" />
+    </template>
+    <template>
+      <SJSearchField label="포맷코드">
+        <SJInput id="md1" v-model="search.md1" name="md1" />
+      </SJSearchField>
+      <SJSearchField label="포맷명">
+        <SJInput id="md2" v-model="search.md2" name="md2" />
+      </SJSearchField>
+    </template>
+    <template #bodyTitle>
+      <SJTitle title="수량포맷정보">
+        <button class="btn-white-bg">
+          추가
+        </button>
+        <button class="btn-white-bg">
+          삭제
+        </button>
+      </SJTitle>
+    </template>
+    <template #body>
+      <SJGrid
+        ref="grid"
+        v-model="grid.data"
+        :columns="grid.columns"
+      />
+    </template>
+  </SJSearchOneLayout>
+</template>
+
+<script>
+import { ACTION } from '~/mixins'
+
+export default {
+  mixins: [ACTION],
+  data () {
+    return {
+      search: {},
+      grid: {
+        data: {},
+        columns: [
+          { name: '포맷코드', width: 150 },
+          { name: '포맷명', width: 150 },
+          { name: '소수점자리수', width: 150 },
+          { name: '최소단위', width: 150 },
+          { name: '올림구분', width: 150 },
+          { name: '포맷', width: 150 }
+        ]
+      }
+    }
+  },
+  created () {
+  },
+  methods: {
+    ACTION_REGISTRY () {
+      const fnc = this
+      return {
+        async searchClick () {
+          await fnc.$notify.info('조회')
+        },
+        async saveClick () {
+          await fnc.$notify.success('저장')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
