adc6e1b2d00cd693244ec7172725c7bbe40da83b
jpkim <jpkim@seojinsys.co.kr>
2023-07-19 15:17:55 +0900

    영업관리 팝업 - 품목조회, 이력조회

---
+++ components/modal/sd/item.vue
@@ -0,0 +1,84 @@
+<template>
+  <SJModal ref="modal" size="modal-xl">
+    <template #title>
+      품목조회
+    </template>
+
+    <template #default>
+      <SJSearchOneLayout disabled-navigator-wrap>
+        <template #default>
+          <SJSearchField label="거래처">
+            <SJInput id="searchCoCode" v-model="search.coCode" name="searchCoCode" />
+          </SJSearchField>
+          <SJSearchField label="품목">
+            <SJInput id="searchCoCode" v-model="search.item" name="searchItem" />
+          </SJSearchField>
+        </template>
+        <template #body>
+          <SJGrid ref="grid" v-model="grid.data" :columns="grid.columns" />
+        </template>
+      </SJSearchOneLayout>
+    </template>
+
+    <template #footer>
+      <button type="button" class="btn btn-secondary" @click="close()">
+        Close
+      </button>
+    </template>
+  </SJModal>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      search: {},
+      grid: {
+        data: {},
+        options: {
+          rowHeaders: ['checkbox', 'rowNum']
+        },
+        columns: [
+          { name: '거래처코드', width: 100 },
+          { name: '거래처명', width: 100 },
+          { name: '품목유형', width: 100 },
+          { name: '품목코드', width: 100 },
+          { name: '품목명', width: 100 },
+          { name: '품목코드', width: 100 },
+          { name: 'REV', width: 100 },
+          { name: '규격', width: 100 },
+          { name: 'CE!여부', width: 100 },
+          { name: '크리티컬여부', width: 100 },
+          { name: '크리티컬등급', width: 100 },
+          { name: '품목대분류', width: 100 },
+          { name: '품목소분류', width: 100 },
+          { name: '기준단위', width: 100 }
+        ]
+      },
+      resolve: null,
+      reject: null
+    }
+  },
+  created () {
+    this.options = [{ value: 1, text: '사과' }, { value: 2, text: '바나나' }]
+  },
+  methods: {
+    open () {
+      this.$refs.modal.show()
+      return new Promise((resolve, reject) => {
+        this.resolve = resolve
+        this.reject = reject
+      })
+    },
+    close () {
+      // this.$refs.form.reset()
+
+      this.$refs.modal.hide()
+      this.resolve(false) // response의 응답
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
