3b082559bc333a4471bfc1bd7b71734f263db699
jpkim <jpkim@seojinsys.co.kr>
2023-07-20 14:56:30 +0900

    영업관리>영업활동관리 생성

---
+++ components/modal/sd/sd210.vue
@@ -0,0 +1,160 @@
+<template>
+  <SJModal ref="modal">
+    <template #title>
+      영업활동
+    </template>
+
+    <template #default>
+      <SJForm ref="form">
+        <SJTitle title="관리년월" />
+        <SJFormRow>
+          <SJFormField label="연도">
+            <SJSelect id="searchCoType" name="연도" :options="common.USE_YN" rules="required" />
+          </SJFormField>
+          <SJFormField label="월">
+            <SJSelect id="searchCoType" name="월" :options="common.USE_YN" rules="required" />
+          </SJFormField>
+        </SJFormRow>
+        <SJTitle title="사업정보" />
+        <SJFormRow>
+          <SJFormField label="사업명">
+            <SJInput id="coCode" v-model="inputData.CO_CODE" name="사업명" rules="required" />
+          </SJFormField>
+        </SJFormRow>
+        <SJFormRow>
+          <SJFormField label="고객사">
+            <SJInput id="coCode" v-model="inputData.CO_CODE" name="고객사" rules="required" />
+          </SJFormField>
+          <SJFormField label="담당자명">
+            <SJInput id="coCode" v-model="inputData.CO_CODE" name="담당자명" rules="required" />
+          </SJFormField>
+        </SJFormRow>
+        <SJFormRow>
+          <SJFormField label="직급">
+            <SJInput id="coCode" v-model="inputData.CO_CODE" name="직급" />
+          </SJFormField>
+          <SJFormField label="직책">
+            <SJInput id="coCode" v-model="inputData.CO_CODE" name="직책" />
+          </SJFormField>
+        </SJFormRow>
+        <SJFormRow>
+          <SJFormField label="공고일">
+            <SJDatePicker
+              id="sentStartDtm"
+              v-model="inputData.sentStartDtm"
+              name="공고일"
+              show-current="true"
+              disabled-validation
+              rules="required"
+            />
+          </SJFormField>
+        </SJFormRow>
+        <SJFormRow>
+          <SJFormField label="RFP등관련자료">
+            <SJFileUpload id="form_files" v-model="inputData.files" name="files" @fileDownload="fileDownload" />
+          </SJFormField>
+        </SJFormRow>
+        <SJTitle title="영업정보" />
+        <SJFormRow>
+          <SJFormField label="영업담당자명">
+            <SJInput id="coCode" v-model="inputData.CO_CODE" name="영업담당자명" rules="required" />
+          </SJFormField>
+          <SJFormField label="직급">
+            <SJInput id="coCode" v-model="inputData.CO_CODE" name="직급" rules="required" />
+          </SJFormField>
+        </SJFormRow>
+        <SJFormRow>
+          <SJFormField label="수주금액">
+            <SJInput id="coCode" v-model="inputData.CO_CODE" name="수주금액" rules="required" />
+          </SJFormField>
+          <SJFormField label="처리결과">
+            <SJSelect id="searchCoType" name="searchCoType" :options="common.USE_YN" rules="required" />
+          </SJFormField>
+        </SJFormRow>
+        <SJFormRow>
+          <SJFormField label="매출발생예상일">
+            <SJDatePicker
+              id="sentStartDtm"
+              v-model="inputData.sentStartDtm"
+              :name="$t('page.system.00083')"
+              show-current="true"
+              disabled-validation
+            />
+          </SJFormField>
+        </SJFormRow>
+        <SJFormRow>
+          <SJFormField label="비고">
+            <SJTextarea
+              id="textarea"
+              v-model="inputData.content"
+              name="textarea"
+            />
+          </SJFormField>
+        </SJFormRow>
+      </SJForm>
+    </template>
+
+    <template #footer>
+      <button type="button" class="btn btn-secondary" @click="close()">
+        Close
+      </button>
+      <button type="button" class="btn btn-primary" @click="save()">
+        Save changes
+      </button>
+    </template>
+  </SJModal>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      current: null,
+      counter: 10,
+      test: 'test',
+      selected: null,
+      options: null,
+      common: {},
+      inputData: { CO_CODE: null },
+      item: {
+        title: null,
+        content: null,
+        upload: []
+      },
+      resolve: null,
+      reject: null
+    }
+  },
+  created () {
+    this.options = [{ value: 1, text: '사과' }, { value: 2, text: '바나나' }]
+  },
+  methods: {
+    open () {
+      this.$refs.modal.show()
+      return new Promise((resolve, reject) => {
+        this.resolve = resolve
+        this.reject = reject
+      })
+    },
+    close () {
+      this.$refs.form.reset()
+
+      this.$refs.modal.hide()
+      this.resolve(false) // response의 응답
+    },
+    async save () {
+      const result = await this.$refs.form.validate()
+      if (result) {
+        this.$refs.modal.hide()
+        this.resolve(true)
+      }
+    },
+    fileDownload (fileId) {
+      this.$api.system.program.downloadHelp(fileId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/sd/sd/sd200.vue
@@ -0,0 +1,108 @@
+<template>
+  <SJSearchOneLayout>
+    <template #master-btn>
+      <SJPageButtons :action="ACTION" />
+      <Sd210 ref="sd210Pop" />
+    </template>
+    <template #default>
+      <SJSearchField label="영업성명">
+        <SJInput id="searchCoCode" v-model="search.coCode" name="searchCoCode" />
+      </SJSearchField>
+      <SJSearchField label="상태">
+        <SJSelect id="searchCoType" name="searchCoType" :options="common.USE_YN" />
+      </SJSearchField>
+      <SJSearchField label="결과">
+        <SJSelect id="searchCoType" name="searchCoType" :options="common.USE_YN" />
+      </SJSearchField>
+      <SJSearchField label="사업명">
+        <SJInput id="searchCoName" v-model="search.coName" name="searchCoName" />
+      </SJSearchField>
+      <SJSearchField label="구분">
+        <SJSelect id="searchCoType" name="searchCoType" :options="common.USE_YN" />
+        <SJDatePicker
+          id="sentStartDtm"
+          v-model="search.sentStartDtm"
+          :name="$t('page.system.00083')"
+          show-current="true"
+          disabled-validation
+        />
+        <div>
+          ~
+        </div>
+        <SJDatePicker
+          id="sentEndDtm"
+          v-model="search.sentEndDtm"
+          :name="$t('page.system.00083')"
+          disabled-validation
+        />
+      </SJSearchField>
+    </template>
+    <template #bodyTitle>
+      <SJTitle title="목록">
+        <button class="btn-white-bg" @click="sd210Pop">
+          신규
+        </button>
+      </SJTitle>
+    </template>
+    <template #body>
+      <SJGrid ref="grid" v-model="grid.data" :columns="grid.columns" />
+    </template>
+  </SJSearchOneLayout>
+</template>
+
+<script>
+// 메뉴 화면
+// 기능권한버튼을 사용하는 경우
+import { MENU, ACTION } from '~/mixins'
+
+export default {
+  mixins: [MENU, ACTION],
+  data () {
+    return {
+      common: {},
+      search: {},
+      grid: {
+        data: {},
+        options: {
+          rowHeaders: ['checkbox', 'rowNum']
+        },
+        columns: [
+          { name: '상태' },
+          { name: '관리년월' },
+          { name: '결과' },
+          { name: '공고일' },
+          { name: '마감일시' },
+          { name: '사업명' },
+          { name: '고객사' },
+          { name: '영업성명' },
+          { name: '직급' },
+          { name: '등록자' },
+          { name: '등록일시' },
+          { name: '수정자' },
+          { name: '수정일시' }
+        ]
+      }
+    }
+  },
+  methods: {
+    async sd210Pop () {
+      // this.$notify.info('품목별/거래처별 조회 팝업 호출')
+      const result = await this.$refs.sd210Pop.open()
+      if (result) {
+        console.log('check')
+      }
+    },
+    ACTION_REGISTRY () {
+      const fnc = this
+      return {
+        async searchClick () {
+          await fnc.$notify.info('조회')
+        },
+        async saveClick () {
+          await fnc.$notify.success('저장')
+        }
+      }
+    }
+  }
+}
+</script>
