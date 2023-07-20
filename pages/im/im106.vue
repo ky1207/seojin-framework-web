2caaee4ab7c02d7a0d6453759cc9b92570fb710b
kskim <kskim@seojinsys.co.kr>
2023-07-20 15:54:17 +0900

    기타이동등록 재고실사등록

--- pages/im/im106.vue
+++ pages/im/im106.vue
@@ -1,10 +1,101 @@
 <template>
-  <div>im106</div>
+  <SJSearchTBLayout>
+    <template #master-btn>
+      <SJPageButtons :action="ACTION" />
+    </template>
+    <template #default>
+      <SJSearchField label="사업장">
+        <SJSelect
+          id="search1"
+          disabled-first-message
+          name="search1"
+          :options="[{text: 'A사업부',value: 'A사업부'},{text: 'B사업부',value: 'B사업부'}]"
+        />
+      </SJSearchField>
+      <SJSearchField label="등록일">
+        TODO:기간검색
+      </SJSearchField>
+      <SJSearchField label="이동번호">
+        <SJInput id="search3" name="search2" />
+      </SJSearchField>
+      <SJSearchField label="품목">
+        <SJInput id="search7" name="search7" />
+      </SJSearchField>
+    </template>
+    <template #topTitle>
+      <SJTitle title="기타이동" />
+    </template>
+    <template #top>
+      <SJGrid ref="grid" v-model="grid.data" :columns="grid.columns" :options="grid.options" />
+    </template>
+
+    <template #bottomTitle>
+      <SJTitle title="기타이동 상세" />
+    </template>
+    <template #bottom>
+      <SJGrid ref="grid_detail" v-model="grid_detail.data" :columns="grid_detail.columns" />
+    </template>
+  </SJSearchTBLayout>
 </template>
 
 <script>
+import { ACTION } from '~/mixins'
 export default {
-
+  mixins: [ACTION],
+  data () {
+    return {
+      grid: {
+        data: {},
+        columns: [
+          { name: '이동번호', width: 100 },
+          { name: '등록일', width: 150 },
+          { name: '사용자명', width: 100 },
+          { name: '이동유형', width: 100 },
+          { name: '이동전창고', width: 150 },
+          { name: '이동후창고', width: 150 },
+          { name: '이동전품목', width: 150 },
+          { name: '이동후품목', width: 150 },
+          { name: '품목(수)', width: 100 },
+          { name: '수정일', width: 150 },
+          { name: '수정자', width: 100 }
+        ],
+        options: {
+          bodyHeight: 250
+        }
+      },
+      grid_detail: {
+        data: {},
+        columns: [
+          { name: '품목유형', width: 100 },
+          { name: '품목코드', width: 120 },
+          { name: '품목명', width: 200 },
+          { name: 'REV', width: 100 },
+          { name: '규격', width: 100 },
+          { name: 'CE!여부', width: 100 },
+          { name: '크리티컬여부', width: 100 },
+          { name: '크리티컬등급', width: 100 },
+          { name: '기준단위', width: 100 },
+          { name: 'LOT', width: 100 },
+          { name: '후LOT', width: 100 },
+          { name: '이동수량', width: 100 },
+          { name: '현재고', width: 100 },
+          { name: '비고', width: 300 }
+        ]
+      }
+    }
+  },
+  methods: {
+    ACTION_REGISTRY () {
+      const fnc = this
+      return {
+        f1Label: '신규',
+        f2Label: '수정',
+        async searchClick () {
+          await fnc.$notify.info('조회')
+        }
+      }
+    }
+  }
 }
 </script>
 
--- pages/im/im107.vue
+++ pages/im/im107.vue
@@ -1,10 +1,108 @@
 <template>
-  <div>im107</div>
+  <SJSearchTBLayout>
+    <template #master-btn>
+      <SJPageButtons :action="ACTION" />
+    </template>
+    <template #default>
+      <SJSearchField label="사업장">
+        <SJSelect
+          id="search1"
+          disabled-first-message
+          name="search1"
+          :options="[{text: 'A사업부',value: 'A사업부'},{text: 'B사업부',value: 'B사업부'}]"
+        />
+      </SJSearchField>
+      <SJSearchField label="등록일">
+        TODO:기간검색
+      </SJSearchField>
+      <SJSearchField label="실사유형">
+        <SJSelect
+          id="search3"
+          disabled-first-message
+          name="search3"
+          :options="[{text: '전체',value: '전체'},{text: '실사조정',value: '실사조정'}]"
+        />
+      </SJSearchField>
+      <SJSearchField label="실사번호">
+        <SJInput id="search4" name="search4" />
+      </SJSearchField>
+      <SJSearchField label="품목">
+        <SJInput id="search5" name="search5" />
+      </SJSearchField>
+    </template>
+    <template #topTitle>
+      <SJTitle title="기타이동" />
+    </template>
+    <template #top>
+      <SJGrid ref="grid" v-model="grid.data" :columns="grid.columns" :options="grid.options" />
+    </template>
+
+    <template #bottomTitle>
+      <SJTitle title="기타이동 상세" />
+    </template>
+    <template #bottom>
+      <SJGrid ref="grid_detail" v-model="grid_detail.data" :columns="grid_detail.columns" />
+    </template>
+  </SJSearchTBLayout>
 </template>
 
 <script>
+import { ACTION } from '~/mixins'
 export default {
-
+  mixins: [ACTION],
+  data () {
+    return {
+      grid: {
+        data: {},
+        columns: [
+          { name: '실사번호', width: 100 },
+          { name: '등록일', width: 150 },
+          { name: '창고', width: 200 },
+          { name: '사용자명', width: 150 },
+          { name: '품목(수)', width: 100 },
+          { name: '수정일', width: 150 },
+          { name: '수정자', width: 100 }
+        ],
+        options: {
+          bodyHeight: 250
+        }
+      },
+      grid_detail: {
+        data: {},
+        columns: [
+          { name: '종료여부', width: 100 },
+          { name: '품목유형', width: 100 },
+          { name: '품목코드', width: 120 },
+          { name: '품목명', width: 200 },
+          { name: 'REV', width: 100 },
+          { name: '규격', width: 100 },
+          { name: 'CE!여부', width: 100 },
+          { name: '크리티컬여부', width: 100 },
+          { name: '크리티컬등급', width: 100 },
+          { name: '기준단위', width: 100 },
+          { name: '거래처', width: 150 },
+          { name: 'LOT', width: 100 },
+          { name: '전산재고', width: 100 },
+          { name: '실사수량', width: 100 },
+          { name: '차이수량', width: 100 },
+          { name: '조정수량', width: 100 },
+          { name: '비고', width: 300 }
+        ]
+      }
+    }
+  },
+  methods: {
+    ACTION_REGISTRY () {
+      const fnc = this
+      return {
+        f1Label: '신규',
+        f2Label: '수정',
+        async searchClick () {
+          await fnc.$notify.info('조회')
+        }
+      }
+    }
+  }
 }
 </script>
 
