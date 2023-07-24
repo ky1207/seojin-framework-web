1a543c9b8f40c2a5027d788caf87bf0ce45935c6
yeonji <yeonji@seojinsys.co.kr>
2023-07-24 11:19:24 +0900

    1. 출하검사(oq100,101,102)페이지 생성 2. 출하검사등록(oq100) 모달 생성 3. 그룹별품목 일괄등록(md101) 모달 생성

---
+++ components/modal/qm/md101.vue
@@ -0,0 +1,151 @@
+<template>
+  <SJModal ref="modal" size="modal-xl">
+    <template #title>
+      그룹별품목 일괄등록
+    </template>
+    <template #button>
+      <button class="btn-gray-bg">
+        조회
+      </button>
+      <button class="btn-blue-bg">
+        저장
+      </button>
+      <button class="btn-white-bg" @click="close">
+        닫기
+      </button>
+    </template>
+    <template #default>
+      <SJTitle title="기준정보" />
+      <SJSearchOneLayout disabled-navigator-wrap>
+        <template #default>
+          <SJSearchField label="사업장">
+            <SJSelect
+              id="md1"
+              name="사업장"
+              :options="MD_01"
+              class="md-select"
+            />
+          </SJSearchField>
+          <SJSearchField label="품목유형">
+            <SJSelect
+              id="md2"
+              name="품목유형"
+              :options="MD_02"
+              class="md-select"
+            />
+          </SJSearchField>
+          <SJSearchField label="품목분류">
+            TODO:SELECTSELECT
+          </SJSearchField>
+          <SJSearchField label="그룹코드">
+            <SJInput id="md3" v-model="search.md3" name="그룹코드" class="md-input" />
+          </SJSearchField>
+          <SJSearchField label="품목">
+            TODO:SELECTSELECT
+          </SJSearchField>
+          <SJSearchField label="생성일">
+            <SJDatePicker
+              id="md4"
+              name="생성일"
+            />
+            <div>~</div>
+            <SJDatePicker
+              id="md4"
+              name="생성일"
+            />
+          </SJSearchField>
+        </template>
+        <template #body>
+          <SJTitle title=" EXCEL (행추가 후 항목수에 맡게 붙여넣기, key값 새로 부여-신규등록시 사용)">
+            <button class="btn-blue-bg">
+              VALIDATION
+            </button>
+            <button class="btn-white-bg">
+              추가
+            </button>
+            <button class="btn-white-bg">
+              삭제
+            </button>
+          </SJTitle>
+          <SJGrid
+            ref="grid"
+            v-model="grid.data"
+            :columns="grid.columns"
+          />
+        </template>
+      </SJSearchOneLayout>
+    </template>
+  </SJModal>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      search: {},
+      resolve: null,
+      reject: null,
+      grid: {
+        data: {},
+        columns: [
+          { name: '검사그룹코드', width: 100 },
+          { name: '품목코드', width: 100 },
+          { name: '비고', width: 200 },
+          { name: '정렬', width: 80 },
+          { name: '사용여부', width: 80 },
+          { name: '생성일', width: 100 }
+        ]
+      }
+    }
+  },
+  created () {
+    this.options = []
+    this.MD_01 = this.getMD01()
+    this.MD_02 = this.getMD02()
+  },
+  methods: {
+    open () {
+      this.$refs.modal.show()
+    },
+    close () {
+      this.$refs.modal.hide()
+    },
+    getMD01 () {
+      return [{
+        text: 'A사업부',
+        value: 'A사업부'
+      }, {
+        text: 'B사업부',
+        value: 'B사업부'
+      }
+      ]
+    },
+    getMD02 () {
+      return [
+        {
+          text: '제품',
+          value: '제품'
+        },
+        {
+          text: '반제품',
+          value: '반제품'
+        },
+        {
+          text: '원재료',
+          value: '원재료'
+        },
+        {
+          text: '저장품',
+          value: '저장품'
+        },
+        {
+          text: '...',
+          value: '...'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style>
+</style>
---
+++ pages/qm/oq/oq101.vue
@@ -0,0 +1,157 @@
+<template>
+  <SJSearchOneLayout>
+    <template #master-btn>
+      <SJPageButtons :action="ACTION" />
+      <oq100 v-if="showModal" ref="oqModal" @close="showModal = false" />
+    </template>
+    <template #default>
+      <SJSearchField label="사업장">
+        <SJSelect
+          id="md1"
+          name="사업장"
+          :options="MD_01"
+        />
+      </SJSearchField>
+      <SJSearchField label="요청일">
+        <SJDatePicker
+          id="md2"
+          name="요청일"
+        />
+        <div>~</div>
+        <SJDatePicker
+          id="md2"
+          name="요청일"
+        />
+      </SJSearchField>
+      <SJSearchField label="검사번호">
+        <SJInput id="md3" v-model="search.md3" name="검사번호" />
+      </SJSearchField>
+      <SJSearchField label="LOT번호">
+        <SJInput id="md4" v-model="search.md4" name="LOT번호" />
+      </SJSearchField>
+      <SJSearchField label="품목분류">
+        <SJSelect
+          id="md5"
+          name="품목분류"
+          :options="MD_02"
+        />
+      </SJSearchField>
+      <SJSearchField label="품목">
+        TODO:SELECTINPUT
+      </sjsearchfield>
+      <SJSearchField label="FCID">
+        <SJInput id="md6" v-model="search.md6" name="FCID" />
+      </SJSearchField>
+    </template>
+    <template #body>
+      <SJTitle title="출하검사" />
+      <SJGrid
+        ref="grid"
+        v-model="grid.data"
+        :columns="grid.columns"
+      />
+    </template>
+  </SJSearchOneLayout>
+</template>
+
+<script>
+import { ACTION } from '~/mixins'
+import Oq100 from '~/components/modal/qm/oq100.vue'
+export default {
+  components: { Oq100 },
+  mixins: [ACTION],
+  data () {
+    return {
+      showModal: false,
+      search: {},
+      grid: {
+        data: {},
+        columns: [
+          { name: '검사일자', width: 80 },
+          { name: '검사번호', width: 100 },
+          { name: '검사#', width: 150 },
+          { name: '품목유형', width: 80 },
+          { name: 'FCID', width: 150 },
+          { name: '검사결과', width: 80 },
+          { name: 'Fab', width: 100 },
+          { name: '고객PO번호', width: 80 },
+          { name: '품목명', width: 100 },
+          { name: 'REV', width: 80 },
+          { name: '규격', width: 100 },
+          { name: '기준단위', width: 80 },
+          { name: '검사방법', width: 80 },
+          { name: 'LOT', width: 80 },
+          { name: '요청수량', width: 80 },
+          { name: '합격', width: 80 },
+          { name: '불량', width: 80 },
+          { name: '내용', width: 100 },
+          { name: 'CE!여부', width: 80 },
+          { name: '크리티컬여부', width: 80 },
+          { name: '크리티컬등급', width: 80 },
+          { name: 'Z-TAG Serial', width: 80 }
+        ]
+      }
+    }
+  },
+  created () {
+    this.MD_01 = this.getMD01()
+    this.MD_02 = this.getMD02()
+  },
+  methods: {
+    ACTION_REGISTRY () {
+      const fnc = this
+      return {
+        async searchClick () {
+          await fnc.$notify.info('조회')
+        },
+        f1Click: async () => {
+          this.showModal = true
+          const result = await this.$refs.oqModal.open()
+          if (result) {
+            console.log(result)
+          }
+        },
+        f1Label: '출하검사'
+      }
+    },
+    getMD01 () {
+      return [{
+        text: 'A사업부',
+        value: 'A사업부'
+      }, {
+        text: 'B사업부',
+        value: 'B사업부'
+      }
+      ]
+    },
+    getMD02 () {
+      return [
+        {
+          text: '전체',
+          value: '전체'
+        },
+        {
+          text: '미검사',
+          value: '미검사'
+        },
+        {
+          text: '합격',
+          value: '합격'
+        },
+        {
+          text: '부분합격',
+          value: '부분합격'
+        },
+        {
+          text: '불합격',
+          value: '불합격'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
