18535cf322fc05fc7dc8a9aff865ccc843d4b311
jpkim <jpkim@seojinsys.co.kr>
2023-07-25 09:21:17 +0900

    영업관리 > 기준정보관리 경로 수정

---
+++ pages/sd/cm/sd101.vue
@@ -0,0 +1,121 @@
+<template>
+  <SJSearchOneLayout>
+    <template #master-btn>
+      <SJPageButtons :action="ACTION" />
+      <SdItem ref="itemPop" />
+      <SdHistory ref="historyPop" />
+    </template>
+    <template #default>
+      <SJSearchField label="회사">
+        <SJSelect
+          id="searchCompany"
+          name=""
+          :options="common.USE_YN"
+          disabled-first-message
+        />
+      </SJSearchField>
+      <SJSearchField label="거래처코드">
+        <SJInput id="searchCoCode" v-model="search.coCode" name="searchCoCode" />
+      </SJSearchField>
+      <SJSearchField label="거래처명">
+        <SJInput id="searchCoName" v-model="search.coName" name="searchCoName" />
+      </SJSearchField>
+      <SJSearchField label="품목유형">
+        <SJSelect id="searchCoType" name="searchCoType" :options="common.USE_YN" />
+      </SJSearchField>
+      <SJSearchField label="품목분류">
+        <SJSelect id="searchCoType" name="searchCoType" :options="common.USE_YN" />
+      </SJSearchField>
+      <SJSearchField label="단가유무">
+        <SJSelect id="searchCoType" name="searchCoType" :options="common.USE_YN" />
+      </SJSearchField>
+      <SJSearchField label="사용여부">
+        <SJSelect id="searchCoType" name="searchCoType" :options="common.USE_YN" />
+      </SJSearchField>
+    </template>
+    <template #bodyTitle>
+      <SJTitle title="목록">
+        <button class="btn-white-bg" @click="itemPop">
+          품목
+        </button>
+        <button class="btn-white-bg" @click="historyPop">
+          이력
+        </button>
+      </SJTitle>
+    </template>
+    <template #body>
+      <SJGrid ref="grid" v-model="grid.data" :columns="grid.columns" />
+    </template>
+  </SJSearchOneLayout>
+</template>
+
+<script>
+// 메뉴 화면
+// 기능권한버튼을 사용하는 경우
+import { MENU, ACTION } from '~/mixins'
+
+export default {
+  mixins: [MENU, ACTION],
+  data () {
+    return {
+      common: {},
+      search: {},
+      grid: {
+        data: {},
+        options: {
+          rowHeaders: ['checkbox', 'rowNum']
+        },
+        columns: [
+          { name: '거래처코드' },
+          { name: '거래처명' },
+          { name: '품목유형' },
+          { name: '품목코드' },
+          { name: '품목명' },
+          { name: '품목코드' },
+          { name: 'REV' },
+          { name: '규격' },
+          { name: 'CE!여부' },
+          { name: '크리티컬여부' },
+          { name: '크리티컬등급' },
+          { name: '기준단위' },
+          { name: '통화' },
+          { name: '구간시작' },
+          { name: '구간종료' },
+          { name: '판매단가' },
+          { name: '적용시작일' },
+          { name: '적용종료일' },
+          { name: '사용여부' },
+          { name: '비고' }
+        ]
+      }
+    }
+  },
+  methods: {
+    async itemPop () {
+      // this.$notify.info('품목별/거래처별 조회 팝업 호출')
+      const result = await this.$refs.itemPop.open()
+      if (result) {
+        console.log('check')
+      }
+    },
+    async historyPop () {
+      // 이력팝업
+      const result = await this.$refs.historyPop.open()
+      if (result) {
+        console.log('check')
+      }
+    },
+    ACTION_REGISTRY () {
+      const fnc = this
+      return {
+        async searchClick () {
+          await fnc.$notify.info('조회')
+        },
+        async saveClick () {
+          await fnc.$notify.success('저장')
+        }
+      }
+    }
+  }
+}
+</script>
